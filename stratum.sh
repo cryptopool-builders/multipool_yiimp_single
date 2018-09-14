e65c62cf7db33275c71f083aac8d490c1d81bfa9
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 12:27:32 -0400

    Create stratum.sh

---
+++ stratum.sh
@@ -0,0 +1,58 @@
+source /etc/functions.sh
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+
+echo Building blocknotify and stratum...
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify
+blckntifypass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
+hide_output sudo make
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/iniparser
+hide_output sudo make
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
+sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/Makefile
+hide_output sudo make
+
+echo Building stratum folder structure and copying files...
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
+sudo cp -a config.sample/. $STORAGE_ROOT/yiimp/site/stratum/config
+sudo cp -r stratum $STORAGE_ROOT/yiimp/site/stratum
+sudo cp -r run.sh $STORAGE_ROOT/yiimp/site/stratum
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify/blocknotify $STORAGE_ROOT/yiimp/site/stratum
+
+sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/config/run.sh
+
+echo '#!/bin/bash
+source /etc/multipool.conf
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+ulimit -n 10240
+ulimit -u 10240
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum
+while true; do
+./stratum config/$1
+sleep 2
+done
+exec bash' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/config/run.sh >/dev/null 2>&1
+
+sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/config/run.sh
+
+sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/run.sh
+
+echo '#!/bin/bash
+source /etc/multipool.conf
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum/config/ && ./run.sh $*
+' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/run.sh >/dev/null 2>&1
+sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/run.sh
+
+echo Updating stratum config files with database connection info...
+cd $STORAGE_ROOT/yiimp/site/stratum/config
+sudo sed -i 's/password = tu8tu5/password = '$blckntifypass'/g' *.conf
+sudo sed -i 's/server = yaamp.com/server = '$StratumURL'/g' *.conf
+sudo sed -i 's/host = yaampdb/host = localhost/g' *.conf
+sudo sed -i 's/database = yaamp/database = yiimpfrontend/g' *.conf
+sudo sed -i 's/username = root/username = stratum/g' *.conf
+sudo sed -i 's/password = patofpaq/password = '$StratumUserDBPassword'/g' *.conf
+
+echo Stratum build complete...
+cd ~/Multi-Pool-Installer/install/yiimp-single
