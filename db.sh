3b47c82c0cd3cec44a52ad5f01f195c9f4919326
cryptopool <cryptopool.builders@gmail.com>
2018-11-02 23:00:12 -0400

    Update db.sh

--- db.sh
+++ db.sh
@@ -61,5 +61,14 @@ sudo mysql -u root -p"${DBRootPassword}" yiimpfrontend --force < 2017-11-segwit.
 sudo mysql -u root -p"${DBRootPassword}" yiimpfrontend --force < 2018-01-stratums_ports.sql
 sudo mysql -u root -p"${DBRootPassword}" yiimpfrontend --force < 2018-02-coins_getinfo.sql
 
+
+sudo sed -i 's|max_connections         = 100|max_connections         = 800|g' /etc/mysql/my.cnf
+sudo sed -i 's|thread_cache_size       = 128|thread_cache_size       = 512|g' /etc/mysql/my.cnf
+sudo sed -i 's|tmp_table_size          = 32M|tmp_table_size          = 128M|g' /etc/mysql/my.cnf
+sudo sed -i 's|max_heap_table_size     = 32M|max_heap_table_size     = 128M|g' /etc/mysql/my.cnf
+sudo sed -i 's|wait_timeout            = 600|wait_timeout            = 60|g' /etc/mysql/my.cnf
+sudo sed -i 's|max_allowed_packet      = 16M|max_allowed_packet      = 64M|g' /etc/mysql/my.cnf
+sudo sed -i 's/#bind-address=0.0.0.0/bind-address='$DBInternalIP'/g' /etc/mysql/my.cnf
+restart_service mysql
 echo Database build complete...
 cd $HOME/multipool/yiimp_single
