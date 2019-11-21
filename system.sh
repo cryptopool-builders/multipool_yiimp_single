f5fb39eb456c22f4f15c9e6588370f3075701ddd
cryptopool <cryptopool.builders@gmail.com>
2019-11-20 19:46:52 -0500

    small fix
    small fix

--- system.sh
+++ system.sh
@@ -162,7 +162,7 @@ fi
 echo -e "$GREEN Done...$COL_RESET"
 echo -e " Downloading CryptoPool.builders YiiMP Repo...$COL_RESET"
 hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
-if [[ ("$CoinPort" == "Yes") ]]; then
+if [[ ("$CoinPort" == "yes") ]]; then
 	cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 	sudo git fetch
 	sudo git checkout multi-port
